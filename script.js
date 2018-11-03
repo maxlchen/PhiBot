a959a3397216ff2c40f59402592bc0ce3d6c5672
Max <mlc294@cornell.edu>
2018-11-03 16:54:38 -0400

    Add user input to chat logs

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 // Code goes here
 window.onload = function() {
   function addMsg(msg) {
-    var message = document.querySelectorAll(".chatlogs");
-    var curr = document.createElement("div").setAttribute("class", "chat bot");//document.getElementById("logs");
+    var message = document.getElementById("logs");
+    var curr = document.createElement("div");
+    curr.setAttribute("class", "chat user");//document.getElementById("logs");
     // curr.createElement("div");
-    var text = document.createElement("p").setAttribute("class","chat-message").appendChild(documents.createTextNode(msg));
-    var botimg = new Image();
-    botimg.src = "/img/pdt.jpg";
-    botimg.width = 100;
-    botimg.height = 40;
+    var text = document.createElement("p");//.appendChild(documents.createTextNode(msg));
+    text.setAttribute("class","chat-message");
+    text.innerHTML = msg
+    var botimg = document.createElement("div");
+    botimg.setAttribute("class", "user-photo");
+    var img = new Image();
+    img.src = "/img/smile.png";
+    // botimg.width = 100;
+    // botimg.height = 40;
+    botimg.appendChild(img);
     // var p1 = document.createElement("p");
     // p1.setAttribute("class","chat-message");
     // p1.innerHTML = msg;	
-    text.innerHTML = msg;
+    // text.innerHTML = msg;
     //var p2 = document.createElement("p");
     //p2.setAttribute("class", "route");
     //p2.innerHTML = route;
     curr.appendChild(botimg);
-    curr.appendChild(p1);
-    curr.appendChild(p2);
-    if (message.length) {
-    message[message.length -1].insertAdjacentHTML("afterEnd", curr.outerHTML)
-    } else {
-     document.forms[0].insertAdjacentHTML("afterEnd", curr.outerHTML)
-    }
+    curr.appendChild(text);
+    message.appendChild(curr);
+    // curr.appendChild(p2);
+    // if (message.length) {
+    // message[message.length -1].insertAdjacentHTML("afterEnd", curr.outerHTML)
+    // } else {
+    //  document.forms[0].insertAdjacentHTML("afterEnd", curr.outerHTML)
+    // }
   }
 
   var obj = {
