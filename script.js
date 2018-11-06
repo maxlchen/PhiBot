78832a7dd128e804f200cbe4b85f34b52ccf79fe
Max <mlc294@cornell.edu>
2018-11-05 22:09:57 -0500

    Add Alessandro text

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Code goes here
+var positive = ["Olivia Ure a cutie. I wanna have fun tonight and us cuties together it's magic bound to happen. Life is short, let's live in the moment. Let's party tonight"];
 
 window.onload = function() {
 	// var sentiment = 0;
@@ -17,7 +18,7 @@ window.onload = function() {
     // curr.createElement("div");
     var text = document.createElement("p");//.appendChild(documents.createTextNode(msg));
     text.setAttribute("class","chat-message");
-    text.innerHTML = msg
+    text.innerHTML = msg;
     var botimg = document.createElement("div");
     botimg.setAttribute("class", "user-photo");
     var img = new Image();
@@ -35,6 +36,22 @@ window.onload = function() {
     curr.appendChild(botimg);
     curr.appendChild(text);
     message.appendChild(curr);
+    var positivesentence = positive[Math.floor(Math.random()*positive.length)];
+
+    var bot = document.createElement("div");
+    bot.setAttribute("class", "chat bot");
+    var bottext = document.createElement("p");
+    bottext.setAttribute("class", "chat-message");
+    bottext.innerHTML = positivesentence;
+    var robotimg = document.createElement("div");
+    robotimg.setAttribute("class", "user-photo");
+    var img2 = new Image();
+    img2.src = "./img/pdt.jpg";
+    robotimg.append(img2);
+    bot.appendChild(robotimg);
+    bot.appendChild(bottext);
+    message.appendChild(bot);
+
     // curr.appendChild(p2);
     // if (message.length) {
     // message[message.length -1].insertAdjacentHTML("afterEnd", curr.outerHTML)
